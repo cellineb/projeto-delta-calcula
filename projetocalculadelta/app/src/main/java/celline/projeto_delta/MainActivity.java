30b814034c163f81c2dcdaf406d9f69afef0891d
Celline Bitencourt <cellinebitencourt@gmail.com>
2024-10-05 12:59:53 -0300

    primeiro commit

--- projetocalculadelta/app/src/main/java/celline/projeto_delta/MainActivity.java
+++ projetocalculadelta/app/src/main/java/celline/projeto_delta/MainActivity.java
@@ -1,77 +1,79 @@
 package celline.projeto_delta;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import celline.projeto_delta.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private EditText etA;
+    private EditText etB;
+    private EditText etC;
+    private Button btnCalcular;
+    private TextView tvResultado;
+    private TextView tvResultado2;
+    private TextView tvResultado3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
+
+        etA = findViewById(R.id.etA);
+        etA.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+        etB = findViewById(R.id.etB);
+        etB.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+        etC = findViewById(R.id.etC);
+        etC.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+        btnCalcular = findViewById(R.id.btnCalcular);
+        tvResultado = findViewById(R.id.tvResultado);
+        tvResultado2 = findViewById(R.id.tvResultado2);
+        tvResultado3 = findViewById(R.id.tvResultado3);
+
+
+        tvResultado.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+
+        btnCalcular.setOnClickListener(uncharismatic -> calc());
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void calc() {
+        float A = Float.parseFloat(etA.getText().toString());
+        float B = Float.parseFloat(etB.getText().toString());
+        float C = Float.parseFloat(etC.getText().toString());
+        float resultado = Float.parseFloat(tvResultado.getText().toString());
+        double delta = Math.pow(B, 2) - (4 * A * C);
+        if (delta<0){
+            String naotemraiz = getString(R.string.naotemraiz);
+            tvResultado.setText(naotemraiz);
+            String res = getString(R.string.resultadodelta) + " " + delta;
+            tvResultado2.setText(res);
+        }
+        else {
+            double x1 = ((-B)+(Math.sqrt(delta)))/(2*A);
+            double x2 = ((-B)-(Math.sqrt(delta)))/(2*A);
+            String res = getString(R.string.resultadodelta) + " " + delta;
+            String x1res = getString(R.string.resultadox1) + " " + x1;
+            String x2res = getString(R.string.resultadox2) + " " + x2;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            tvResultado.setText(res);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
+
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
